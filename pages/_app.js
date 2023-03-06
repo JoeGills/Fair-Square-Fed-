a636848ef5e5c7d71078c94e57c4bfb625b1b617
desmati <desmati@gmail.com>
2023-03-06 15:08:33 +0100

    login page

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/global.css";
+
+function FairSquareApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default FairSquareApp;
---
+++ pages/user/sign-in.jsx
@@ -0,0 +1,106 @@
+import Image from "next/image";
+import styles from "./sign-in.module.css";
+
+export default function SignIn() {
+  return (
+    <main className="form-signin text-center">
+      <form>
+        <Image
+          className="mb-4"
+          src="/docs/5.0/assets/brand/bootstrap-logo.svg"
+          alt=""
+          width={72}
+          height={57}
+        />
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            name="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            name="floatingPassword"
+            placeholder="Password"
+          />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me
+          </label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+      </form>
+
+      <style jsx>{`
+        html,
+        body {
+          height: 100%;
+        }
+
+        body {
+          display: flex;
+          align-items: center;
+          padding-top: 40px;
+          padding-bottom: 40px;
+          background-color: #f5f5f5;
+        }
+
+        .form-signin {
+          width: 100%;
+          max-width: 330px;
+          padding: 15px;
+          margin: auto;
+        }
+
+        .form-signin .checkbox {
+          font-weight: 400;
+        }
+
+        .form-signin .form-floating:focus-within {
+          z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+          margin-bottom: -1px;
+          border-bottom-right-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"] {
+          margin-bottom: 10px;
+          border-top-left-radius: 0;
+          border-top-right-radius: 0;
+        }
+
+        .bd-placeholder-img {
+          font-size: 1.125rem;
+          text-anchor: middle;
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          user-select: none;
+        }
+
+        @media (min-width: 768px) {
+          .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+          }
+        }
+      `}</style>
+    </main>
+  );
+}
