6aea1e0d0b2aff0883e97bb0fae4e08da3b72cd8
heartbreathing <li.wang@hyperisland.se>
2023-03-07 14:15:48 +0100

    add card and cardTest

--- components/card.jsx
+++ components/card.jsx
@@ -1,5 +1,45 @@
-export default function Card() {
+// import styles from './card.module.css';
+export default function Card({ title, text, children }) {
   return (
-    <div>Card</div>
+    <>
+      <div
+        class="card"
+        className="text-center"
+        style={{
+          width: '18rem',
+          backgroundColor: '#DED9D9',
+          padding: '1.5rem',
+          margin: '1rem',
+          borderRadius: '0.5rem',
+          textAlign: 'center',
+        }}
+        >
+        {children}
+        <div
+          style={{
+            padding: '1rem',
+            fontSize: '1.2rem',
+          }}>
+          {title}
+        </div>
+        <text>{text}</text>
+      </div>
+    </>
   );
 }
+// export default function Card({ title, text, children }) {
+// return (
+// <>
+//     <div
+//       className='card'
+//     >
+ 
+//     {children}
+//     <div className='card-title'>
+//       {title}
+//     </div>
+//     <text>{text}</text>
+//   </div>
+// </>
+// );
+// }
